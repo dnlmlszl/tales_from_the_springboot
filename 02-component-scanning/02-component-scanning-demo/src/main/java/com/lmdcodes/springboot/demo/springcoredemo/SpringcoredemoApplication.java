fd4562db87c493452175dc086aa2e9a6e8a1b9be
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-18 03:38:09 +0100

    fix: adding listing to SpringBootApplication annotation

--- 02-component-scanning/02-component-scanning-demo/src/main/java/com/lmdcodes/springboot/demo/springcoredemo/SpringcoredemoApplication.java
+++ 02-component-scanning/02-component-scanning-demo/src/main/java/com/lmdcodes/springboot/demo/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,10 @@ package com.lmdcodes.springboot.demo.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.lmdcodes.springboot.demo.springcoredemo",
+		"com.lmdcodes.util"}
+)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
