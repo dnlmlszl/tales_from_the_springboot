d26f6c841ba98538de4f79b6f2da7450875538a5
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-18 19:37:48 +0100

    feat: lazy initialization

---
+++ 02-component-scanning/06-lazy-initialization/src/main/java/com/lmdcodes/springboot/demo/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.lmdcodes.springboot.demo.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In costructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+}
