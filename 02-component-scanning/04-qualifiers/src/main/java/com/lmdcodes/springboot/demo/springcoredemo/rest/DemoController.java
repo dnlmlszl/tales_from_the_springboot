ae1d2943d9df95fe76873da2a3686a64ab33dfcd
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-18 19:00:29 +0100

    feat: qualifier annotation

--- 02-component-scanning/04-qualifiers/src/main/java/com/lmdcodes/springboot/demo/springcoredemo/rest/DemoController.java
+++ 02-component-scanning/04-qualifiers/src/main/java/com/lmdcodes/springboot/demo/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.lmdcodes.springboot.demo.springcoredemo.rest;
 
 import com.lmdcodes.springboot.demo.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,8 @@ public class DemoController {
     // define private field for the dependency
     private Coach myCoach;
 
-    // define setter for injection
     @Autowired
-    public  void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
