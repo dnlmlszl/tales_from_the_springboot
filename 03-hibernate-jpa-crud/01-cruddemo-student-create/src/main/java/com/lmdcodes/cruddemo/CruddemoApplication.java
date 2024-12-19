fa7c340cd7f6ce06e7e543475ee0e0c1b361bd37
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-19 03:15:31 +0100

    feat: setting up create object - DAO interface, DAO implementation class commandline Runner and reate Student method

--- 03-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/lmdcodes/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/lmdcodes/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.lmdcodes.cruddemo;
 
+import com.lmdcodes.cruddemo.dao.StudentDAO;
+import com.lmdcodes.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +14,27 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	// define the command line runner method
+
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@lmdcodes.com");
+
+		// save the student object
+		System.out.println("Saving the student object ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }
---
+++ 03-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/lmdcodes/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.lmdcodes.cruddemo.dao;
+
+import com.lmdcodes.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+}
