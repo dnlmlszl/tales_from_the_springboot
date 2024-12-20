0cbe811799f2db74c4857d04369e215a4d06ebb2
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-20 16:44:06 +0100

    feat: setting up some endoints

--- 04-rest-crud/01-rest-crud-hello-world/src/main/java/com/lmdcodes/restdemo/rest/StudentRestController.java
+++ 04-rest-crud/01-rest-crud-hello-world/src/main/java/com/lmdcodes/restdemo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.lmdcodes.restdemo.rest;
 
 import com.lmdcodes.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +14,32 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the students data ... only once
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
     // define endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
 
-        List<Student> theStudents = new ArrayList<>();
-
-        theStudents.add(new Student("Poornima", "Patel"));
-        theStudents.add(new Student("Mario", "Rossi"));
-        theStudents.add(new Student("Mary", "Smith"));
-
         return theStudents;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // populate theStudents
+        return theStudents.get(studentId);
     }
 }
