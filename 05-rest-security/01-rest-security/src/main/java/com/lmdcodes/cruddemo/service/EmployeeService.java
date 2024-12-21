22455528b33dd734a0b90da35452e1f6cbebeed7
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-12-21 18:21:47 +0100

    feat: project setup - adding UserDetailsManager

---
+++ 05-rest-security/01-rest-security/src/main/java/com/lmdcodes/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.lmdcodes.cruddemo.service;
+
+import com.lmdcodes.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
